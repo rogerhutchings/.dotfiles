7c4218d73ccbda7b648806e0b69ee12a3f293131
Roger Hutchings <roger@hutchin.gs>
2016-10-01 16:58:06 +0100

    Update submodules

--- zsh/zsh/plugins.zsh
+++ zsh/zsh/plugins.zsh
@@ -2,11 +2,9 @@ source ~/.zsh/antigen/antigen.zsh
 
 antigen bundle mafredri/zsh-async
 antigen bundle rogerhutchings/pure
-
 antigen bundle zsh-users/zsh-syntax-highlighting
-
 antigen bundle akoenig/gulp.plugin.zsh
-
 antigen bundle rupa/z
+antigen bundle lukechilds/zsh-nvm
 
 antigen apply
