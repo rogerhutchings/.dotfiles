2c43ac2479ae4bfdae576064257f25c02807120a
Roger Hutchings <roger@hutchin.gs>
2019-12-24 22:07:52 +0000

    Add gitconfig and aliases

---
+++ src/oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,14 @@
+# general
+alias reload='source ~/.zshrc'
+
+# git
+alias git='hub'
+alias g='git'
+alias gs='git status'
+alias gadd='git add'
+alias gpull='git pull'
+
+# ls aliases
+alias ls='gls --indicator-style=slash --color=auto --group-directories-first'
+alias ll='ls -l -v --almost-all --human-readable'
+alias lr='ll -R'
